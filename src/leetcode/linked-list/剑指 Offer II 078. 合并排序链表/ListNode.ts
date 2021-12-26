d22eb2952eb686647aadbba7ceaee2c627a1f12f
儿时 <58261676+childrentime@users.noreply.github.com>
2021-12-27 00:06:16 +0800

    init 合并有序链表

---
+++ src/leetcode/linked-list/剑指 Offer II 078. 合并排序链表/ListNode.ts
@@ -0,0 +1,8 @@
+export default class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
---
+++ src/leetcode/linked-list/剑指 Offer II 078. 合并排序链表/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+import ListNode from './ListNode';
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {}
