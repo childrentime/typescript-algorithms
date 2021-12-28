9c217a0f31fd690a3cf03609f806ffcb0b81449f
s_wulianwen <s_wulianwen@kingsoft.com>
2021-12-28 11:32:48 +0800

    Put aside BitSet a while

--- src/data-structures/bitset/BitSet.ts
+++ src/data-structures/bitset/BitSet.ts
@@ -1,15 +1,16 @@
-const ADDRESS_BITS_PER_WORD = 6;
-const BITS_PER_WORD = 1 << ADDRESS_BITS_PER_WORD;
 
+/**
+ * js 中所有数字都是以双精度浮点数来存储的。即64位；但 当任何数字在进行位运算时 js 内部将其转换成32位有符号整型。
+ * 也就是使用number数组来标识位图的时候，我们只能使用32位，也就是5地址
+ */
+const ADDRESS_BITS_PER_WORD = 5;
+const BITS_PER_WORD = 1 << ADDRESS_BITS_PER_WORD;
+const WORD_MASK = 0xffffffffffffffff;
 const wordIndex = (bitIndex: number) => bitIndex >> ADDRESS_BITS_PER_WORD;
 export default class BitSet {
   // number 最大为2^53-1 一个number是64位
   private _words: number[] = [];
-  private _sizeIsSticky = false;
   private _wordsInUse = 0;
-  private initWords(nbits: number): void {
-    this._words = new Array<number>(wordIndex(nbits - 1) + 1);
-  }
 
   private expandTo(word: number): void {
     let wordsRequired = word + 1;
@@ -23,8 +24,26 @@ export default class BitSet {
     if (this._words.length < wordsRequired) {
       // Allocate larger of doubled size or required size
       this._words.length = Math.max(2 * this._words.length, wordsRequired);
-      this._sizeIsSticky = false;
     }
+  }
+
+  private recalculateWordsInUse(): void {
+    // Traverse the bitset until a used word is found
+    let i;
+    for (i = this._wordsInUse - 1; i >= 0; i--) if (this._words[i] != 0) break;
+
+    this._wordsInUse = i + 1; // The new logical size
+  }
+
+  private checkRange(fromIndex: number, toIndex: number) {
+    if (fromIndex < 0) throw new Error('fromIndex < 0: ' + fromIndex);
+    if (toIndex < 0) throw new Error('toIndex < 0: ' + toIndex);
+    if (fromIndex > toIndex)
+      throw new Error('fromIndex: ' + fromIndex + ' > toIndex: ' + toIndex);
+  }
+
+  private initWords(nbits: number): void {
+    this._words = new Array<number>(wordIndex(nbits - 1) + 1);
   }
 
   constructor(nbits?: number) {
@@ -33,28 +52,16 @@ export default class BitSet {
     }
     const initialCapacity = nbits ?? BITS_PER_WORD;
     this.initWords(initialCapacity);
-    this._sizeIsSticky = false;
   }
 
-    public set(bitIndex: number,  value: boolean): void{
-        if (value)
-            this.set(bitIndex);
-        else
-            this.clear(bitIndex);
-    }
-
-  /**
-   * Sets the bit at the specified index to true.
-   * @param  bitIndex a bit index
-   */
-  public set(bitIndex: number): void {
-    if (bitIndex < 0) throw new Error(`bitIndex < 0 : ${bitIndex}`);
+  public clear(bitIndex: number): void {
+    if (bitIndex < 0) throw new Error('bitIndex < 0: ' + bitIndex);
 
     let word = wordIndex(bitIndex);
-    this.expandTo(word);
+    if (word >= this._wordsInUse) return;
 
-    this._words[word] |= 1 << bitIndex; // Restores invariants
+    this._words[word] &= ~(1 << bitIndex);
+
+    this.recalculateWordsInUse();
   }
-
- 
 }
