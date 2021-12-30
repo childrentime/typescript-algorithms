217bf80607cf37c4f5c80cd870e612fb81dfd0f3
s_wulianwen <s_wulianwen@kingsoft.com>
2021-12-30 15:37:08 +0800

    ADD hot100 两数之和

---
+++ src/leetcode/hot 100/1. 两数之和/firstSubmission.ts
@@ -0,0 +1,13 @@
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    let item = nums[i] as number;
+    if (map.has(item)) {
+      let index = map.get(item) as number;
+      return [index, i];
+    }
+    map.set(target - item, i);
+  }
+
+  throw new Error("this is no answer")
+}
