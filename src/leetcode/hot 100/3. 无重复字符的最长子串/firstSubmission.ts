f732b972efd70da306b3be2481f54e7ab6319bf1
儿时 <58261676+childrentime@users.noreply.github.com>
2021-12-30 22:17:36 +0800

    Add 添加无重复长度的最长子串

--- src/leetcode/hot 100/3. 无重复字符的最长子串/firstSubmission.ts
+++ src/leetcode/hot 100/3. 无重复字符的最长子串/firstSubmission.ts
@@ -1,3 +1,19 @@
+/*
+ * 解题思路：用一个state维护当前不重复子串，记录state的历史变更，找到历史变更中长度最大的
+ */
 export function lengthOfLongestSubstring(s: string): number {
+  let max = 0;
+  let state = '';
+  for (let i = 0; i < s.length; i++) {
+    let item = s[i] as string;
+    const index = state.indexOf(item);
+    if (index !== -1) {
+      // abcd 遇到 a b c d的时候 记录历史变更
+      max = max < state.length ? state.length : max;
+      state = state.substring(index + 1, state.length);
+    }
+    state = state.concat(item);
+  }
 
-};
+  return max > state.length ? max : state.length;
+}
