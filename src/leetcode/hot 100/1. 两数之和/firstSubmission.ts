13ee083d102703823cf4d7bb741c271945f63988
s_wulianwen <s_wulianwen@kingsoft.com>
2021-12-30 16:51:12 +0800

    Add 两数之和

--- src/leetcode/hot 100/1. 两数之和/firstSubmission.ts
+++ src/leetcode/hot 100/1. 两数之和/firstSubmission.ts
@@ -1,4 +1,4 @@
-function twoSum(nums: number[], target: number): number[] {
+export function twoSum(nums: number[], target: number): number[] {
   const map = new Map<number, number>();
   for (let i = 0; i < nums.length; i++) {
     let item = nums[i] as number;
