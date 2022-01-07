9397246c12a4948305820e8a4d06cee4cd0b53da
儿时 <58261676+childrentime@users.noreply.github.com>
2022-01-07 12:34:53 +0800

    ADD unionFind test

---
+++ src/algorithms/union-find/__test__/index.test.ts
@@ -0,0 +1,30 @@
+import QuickFind from '../quick-find/quickFind';
+import QuickUnion from '../quick-union/quickUnion';
+
+describe('UnionFind', () => {
+  it('quickFind', () => {
+    const uf = new QuickFind(10);
+    uf.union(0, 1);
+    uf.union(2, 9);
+    uf.union(3, 7);
+    uf.union(6, 5);
+    uf.union(3, 9);
+    expect(uf.connected(2, 3)).toBe(true);
+    expect(uf.connected(3, 0)).toBe(false);
+    uf.union(1, 3);
+    expect(uf.connected(3, 0)).toBe(true);
+  });
+
+  it('quickUnion', () => {
+    const uf = new QuickUnion(10);
+    uf.union(0, 1);
+    uf.union(2, 9);
+    uf.union(3, 7);
+    uf.union(6, 5);
+    uf.union(3, 9);
+    expect(uf.connected(2, 3)).toBe(true);
+    expect(uf.connected(3, 0)).toBe(false);
+    uf.union(1, 3);
+    expect(uf.connected(3, 0)).toBe(true);
+  });
+});
