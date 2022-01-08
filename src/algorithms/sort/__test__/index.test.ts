e80c3ab02fdef2946058d341ac7c8a099cfac457
儿时 <58261676+childrentime@users.noreply.github.com>
2022-01-09 01:10:44 +0800

    Add SelectSort

--- src/algorithms/sort/__test__/index.test.ts
+++ src/algorithms/sort/__test__/index.test.ts
@@ -1,12 +1,19 @@
 import { quickSort, quickSort2 } from '../quickSort';
+import selectSort from '../selectSort';
 
-describe('QuickSort', () => {
-  it('should sort correct', () => {
+describe('Sort', () => {
+  it('QuickSort', () => {
     const array = [5, 3, 1, 2, 4];
     expect(quickSort(array).toString()).toBe('1,2,3,4,5');
 
     quickSort2(array, 0, 4);
 
+    expect(array.toString()).toBe('1,2,3,4,5');
+  });
+
+  it('SelectSort', () => {
+    const array = [5, 3, 1, 2, 4];
+    selectSort(array);
     expect(array.toString()).toBe('1,2,3,4,5');
   });
 });
