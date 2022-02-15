f12b17c8e6226e1403a586ec5b5d67ecda686623
儿时 <58261676+childrentime@users.noreply.github.com>
2022-02-15 21:41:19 +0800

    Add Palindromic Substrings

--- src/leetcode/hot 100/647. Palindromic Substrings/secondSubmission.ts
+++ src/leetcode/hot 100/647. Palindromic Substrings/secondSubmission.ts
@@ -2,9 +2,9 @@ export function countSubstrings(s: string): number {
   const n = s.length;
   let answer = 0;
   for (let i = 0; i < 2 * n - 1; i++) {
-    let l = i / 2,
+    let l = ~~(i / 2),
       r = l + (i % 2);
-    while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+    while (l >= 0 && r < n && s[l] == s[r]) {
       l--;
       r++;
       answer++;
