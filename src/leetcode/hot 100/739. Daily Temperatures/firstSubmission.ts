12f1fcc44fe5bd823beaba55a53e1b03c8c672ba
儿时 <58261676+childrentime@users.noreply.github.com>
2022-02-18 19:06:42 +0800

    Add Daliy Temperatures

--- src/leetcode/hot 100/739. Daily Temperatures/firstSubmission.ts
+++ src/leetcode/hot 100/739. Daily Temperatures/firstSubmission.ts
@@ -1,1 +1,16 @@
-export function dailyTemperatures(temperatures: number[]): number[] {}
+export function dailyTemperatures(temperatures: number[]): number[] {
+  const result: number[] = [];
+  const stack: number[] = [0];
+  for (let i = 1; i < temperatures.length; i++) {
+    while (temperatures[i]! > temperatures[stack[stack.length - 1]!]!) {
+      const index = stack.pop()!;
+      result[index] = i - index;
+    }
+    stack.push(i);
+  }
+  while (stack.length) {
+    const index = stack.pop()!;
+    result[index] = 0;
+  }
+  return result;
+}
