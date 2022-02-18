37ff84db694d8800c8e2b912b5778b944c302177
儿时 <58261676+childrentime@users.noreply.github.com>
2022-02-18 23:13:24 +0800

    Add Two Sum

---
+++ src/leetcode/top/1. Two Sum/firstSubmission.ts
@@ -0,0 +1,11 @@
+export function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i]!)) {
+      return [i, map.get(nums[i]!)!];
+    } else {
+      map.set(target - nums[i]!, i);
+    }
+  }
+  throw new Error('no answer');
+}
