d5fe291222fa6de5a1087deb415f8ca7382fde17
儿时 <58261676+childrentime@users.noreply.github.com>
2022-02-20 21:52:58 +0800

    Add Merge Two Sorted List

---
+++ src/leetcode/top/21. Merge Two Sorted Lists/ListNode.ts
@@ -0,0 +1,10 @@
+export default class Node {
+  val: number;
+  next: Node | null;
+  random: Node | null;
+  constructor(val?: number, next?: Node, random?: Node) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+    this.random = random === undefined ? null : random;
+  }
+}
