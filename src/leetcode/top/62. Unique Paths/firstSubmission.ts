5abf1e2b799708631091f6bf12601ea85ab94bb7
儿时 <58261676+childrentime@users.noreply.github.com>
2022-02-25 16:18:29 +0800

    Add Unique Paths

---
+++ src/leetcode/top/62. Unique Paths/firstSubmission.ts
@@ -0,0 +1,17 @@
+export function uniquePaths(m: number, n: number): number {
+  const dp: number[][] = Array.from(new Array(m)).map(() =>
+    new Array(n).fill(0)
+  );
+  for (let i = 0; i < m; i++) {
+    dp[i]![0] = 1;
+  }
+  for (let i = 0; i < n; i++) {
+    dp[0]![i] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i]![j] = dp[i - 1]![j]! + dp[i]![j - 1]!;
+    }
+  }
+  return dp[m - 1]![n - 1]!;
+}
