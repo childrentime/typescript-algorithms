36e2a6061c89a8f04f11669d343626b64fdb90d4
儿时 <58261676+childrentime@users.noreply.github.com>
2022-03-01 21:35:04 +0800

    add best time to buy and sell stock

---
+++ src/leetcode/top/121. Best Time to Buy and Sell Stock/firstSubmission.ts
@@ -0,0 +1,12 @@
+export function maxProfit(prices: number[]): number {
+  let answer: number = 0;
+  let min = prices[0]!;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i]! < min) {
+      min = prices[i]!;
+    } else {
+      answer = Math.max(answer, prices[i]! - min);
+    }
+  }
+  return answer;
+}
