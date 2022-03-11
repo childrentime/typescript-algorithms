a175315f202c3f3d97e16c1a0c24edaffe9d738f
儿时 <58261676+childrentime@users.noreply.github.com>
2022-03-11 10:33:52 +0800

    add unnique path II

---
+++ src/leetcode/main/63. Unique Paths II/firstSubmission.ts
@@ -0,0 +1,26 @@
+export function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0]!.length;
+  const dp = Array.from(new Array(m)).map(() => new Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    if (obstacleGrid[i]![0] === 1) {
+      break;
+    }
+    dp[i]![0] = 1;
+  }
+  for (let i = 0; i < n; i++) {
+    if (obstacleGrid[0]![i] !== 1) {
+      break;
+    }
+    dp[0]![i] = 1;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      if (obstacleGrid[i]![j] === 1) {
+        continue;
+      }
+      dp[i]![j] = dp[i]![j - 1] + dp[i - 1]![j];
+    }
+  }
+  return dp[m - 1]![n - 1]!;
+}
