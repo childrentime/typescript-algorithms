ed8683f4b2f86d8d3912578a0a0e1219be6f4bfd
儿时 <58261676+childrentime@users.noreply.github.com>
2022-03-13 14:35:01 +0800

    add throttle

---
+++ src/interview/throttle/index.ts
@@ -0,0 +1,27 @@
+export function throttle<Args extends any[], F extends (...args: Args) => any>(
+  fn: F,
+  wait: number = 500
+) {
+  let canRun = true;
+  return function (this: ThisParameterType<F>, ...args: Args) {
+    if (!canRun) {
+      return;
+    }
+    const context = this;
+    canRun = false;
+    setTimeout(() => {
+      fn.apply(context, args);
+      canRun = true;
+    }, wait);
+  };
+}
+
+function sayHello() {
+  console.log('Hello!');
+}
+
+const throttleSayHello = throttle(sayHello, 5);
+throttleSayHello();
+setTimeout(() => {
+  throttleSayHello();
+}, 10); //hello hello
