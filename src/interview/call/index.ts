34f14d579a0408c2740db769f9b969d1b1d8f7b6
儿时 <58261676+childrentime@users.noreply.github.com>
2022-03-21 10:21:20 +0800

    add call

--- src/interview/call/index.ts
+++ src/interview/call/index.ts
@@ -4,7 +4,7 @@ function MyFunction(this: any, name: string) {
 Function.prototype.call = function <T extends object, A extends any[], R>(
   this: (this: T, ...args: A) => R,
   thisArg: T,
-  args: A
+  ...args: A
 ): R {
   const context: any = thisArg || window;
   const fn = Symbol('fn');
