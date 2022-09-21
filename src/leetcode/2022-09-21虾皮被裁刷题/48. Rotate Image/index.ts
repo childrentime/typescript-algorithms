6363d8f261fc514c0991b2da86f146da09d1f2f9
lianwen wu <lianwenwu@MacBook-Pro.local>
2022-09-22 00:22:23 +0800

    add rotate image

--- src/leetcode/2022-09-21虾皮被裁刷题/48. Rotate Image/index.ts
+++ src/leetcode/2022-09-21虾皮被裁刷题/48. Rotate Image/index.ts
@@ -1,4 +1,21 @@
-export function rotate(matrix: number[][]): void {
-  const m = matrix.length;
-  const n = matrix[0]!.length;
+
+
+// dp[0][0] = dp[2][0]
+// dp[0][1] = dp[1][0]
+// dp[0][2] = dp[0][0]
+// dp[1][0] =dp[2][1]
+// dp[i][j] = dp[n-j-1][i]
+export default function rotate(matrix: number[][]): void {
+  const n = matrix.length;
+
+
+  for(let i =0;i< n >>> 1;i++){
+    for(let j = 0; j< (n+1) >>> 1;j++){
+      const temp = matrix[i]![j]!;
+      matrix[i]![j]! = matrix[n-j-1]![i]!;
+      matrix[n-j-1]![i]! = matrix[n-i-1]![n-j-1]!;
+      matrix[n-i-1]![n-j-1]! = matrix[j]![n-i-1]!
+      matrix[j]![n-i-1] = temp;
+    }
+  }
 }
