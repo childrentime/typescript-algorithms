f413b6b33579c3107c4ad7c7033d8a504337bb7e
berlin <1580940252@qq.com>
2022-09-22 22:58:39 +0800

    add merge sorted array

---
+++ src/leetcode/2022-09-21虾皮被裁刷题/88. Merge Sorted Array/firstSubmission.ts
@@ -0,0 +1,22 @@
+export default function merge(
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): void {
+  let index = m + n - 1;
+  m--;
+  n--;
+
+  while (n >= 0) {
+    if (m >= 0 && nums1[m]! > nums2[n]!) {
+      nums1[index] = nums1[m]!;
+      m--;
+      index--;
+    } else {
+      nums1[index] = nums2[n]!;
+      n--;
+      index--;
+    }
+  }
+}
