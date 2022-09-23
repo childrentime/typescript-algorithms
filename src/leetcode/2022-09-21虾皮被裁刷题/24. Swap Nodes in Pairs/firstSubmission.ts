78bbefc63fbe99aae3959d28832946b4ba21f7dd
berlin <1580940252@qq.com>
2022-09-23 11:33:40 +0800

    add swap nodes

---
+++ src/leetcode/2022-09-21虾皮被裁刷题/24. Swap Nodes in Pairs/firstSubmission.ts
@@ -0,0 +1,24 @@
+import ListNode from '../../hot 100/141. Linked List Cycle/ListNode';
+
+export default function swapPairs(head: ListNode | null): ListNode | null {
+  let pre = null;
+  let cur = head;
+  let p = head;
+
+  for (let i = 0; i < 2; i++) {
+    if (!p) {
+      return head;
+    }
+    p = p.next;
+  }
+
+  for (let i = 0; i < 2; i++) {
+    const next = cur!.next;
+    cur!.next = pre;
+    pre = cur;
+    cur = next;
+  }
+
+  head!.next = swapPairs(cur);
+  return pre;
+}
