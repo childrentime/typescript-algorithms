e00743b1c25e3b2ba213b144f58c9a72bf7ead12
berlin <1580940252@qq.com>
2022-09-23 11:42:51 +0800

    add palind linked list

---
+++ src/leetcode/2022-09-21虾皮被裁刷题/234. Palindrome Linked List/firstSubmission.ts
@@ -0,0 +1,28 @@
+import ListNode from '../../hot 100/141. Linked List Cycle/ListNode';
+
+export default function isPalindrome(head: ListNode | null): boolean {
+  if (!head) {
+    return true;
+  }
+  let fast: ListNode | null = head;
+  let slow: ListNode = head;
+  let pre: ListNode | null = null;
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    const next = slow.next!;
+    slow.next = pre;
+    pre = slow;
+    slow = next;
+  }
+  if (fast) {
+    slow = slow.next!;
+  }
+  while (slow) {
+    if (slow.val !== pre!.val) {
+      return false;
+    }
+    slow = slow.next!;
+    pre = pre!.next!;
+  }
+  return true;
+}
